46428cf759abe3dbfa19e2a263ba0ef3e483b461
mmcdani <mmcdani@ncsu.edu>
2025-11-05 18:22:58 -0500

    added side bar menus

---
+++ Project_2/app.R
@@ -0,0 +1,49 @@
+library(shiny)
+library(tidyverse)
+source("helpers.R")  # now pulls in num_vars and cat_vars
+
+ui <- fluidPage(
+  titlePanel("Mobile Device Usage"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      h2("Select a Subset of Data:"),
+      
+      selectizeInput(
+        "cat_vars",
+        "Select a Categorical Variable:",
+        choices = names(cat_vars),
+        selected = "Gender"
+      ),
+      
+      selectizeInput(
+        "num_vars",
+        "Select a Numeric Variable:",
+        choices = names(num_vars),
+        selected = "Number of Apps Installed"
+      )
+    ),
+    
+    mainPanel(
+      plotOutput("barPlot")
+    )
+  )
+)
+
+server <- function(input, output) {
+  output$barPlot <- renderPlot({
+    cat_col <- cat_vars[[input$cat_vars]]
+    num_col <- num_vars[[input$num_vars]]
+    
+    ggplot(mobile_device_data, aes_string(x = cat_col, y = num_col)) +
+      geom_boxplot(fill = "steelblue", alpha = 0.6) +
+      labs(
+        x = input$cat_vars,
+        y = input$num_vars,
+        title = paste("Distribution of", input$num_vars, "by", input$cat_vars)
+      ) +
+      theme_minimal(base_size = 14)
+  })
+}
+
+shinyApp(ui = ui, server = server)
---
+++ Project_2/helpers.R
@@ -0,0 +1,18 @@
+# numeric variables
+num_vars <- c(
+  "App Usage Time (min/day)" = "app_usage_time",
+  "Screen On Time (hours/day)" = "screen_on_time",
+  "Battery Drain (mAh/day)" = "battery_drain",
+  "Number of Apps Installed" = "num_app_installed",
+  "Data Usage (MB/day)" = "data_usage",
+  "Age" = "age"
+)
+
+# categorical variables
+cat_vars <- c(
+  "User ID" = "user_id",
+  "Device Model" = "device_model",
+  "Operating System" = "operating_system",
+  "Gender" = "gender",
+  "User Behavior Class" = "user_behavior_class"
+)
